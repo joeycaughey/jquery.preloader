d200e9868bf873f19b1ef803c2eec56b57459933
Joey  Caughey <joey@onemethod.com>
2016-04-02 15:59:36 -0400

    Updates

--- jquery.preloader.js
+++ jquery.preloader.js
@@ -1,21 +1,17 @@
-$("body").prepend(
-	$('<div id="preloader">').append(
-		$('<div id="status">').html("&nbsp;")
-	) 
-);
-
-$('#preloader').hide();
-
-
 var Loading = {
 	show: function() { 
+		$("body").prepend(
+			$('<div id="preloader">').append(
+				$('<div id="status">').html("&nbsp;")
+			) 
+		);
 		$("#preloader").show();
 		$('#status').show();
 		//$('body').css({'overflow':'hidden'});
 	}, 
 	hide: function() {
 		$('#status').fadeOut();
-		$('#preloader').delay(5).fadeOut('slow'); 
+		$('#preloader').delay(5).fadeOut('slow').remove();; 
 		$('body').delay(5).css({'overflow':'visible'});
 	}
 }
