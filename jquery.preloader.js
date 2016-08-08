1650220591b9a79efb37a4bdeb4b20166f9ce81f
“Joey <joey@massminority.com>
2016-08-08 13:57:45 -0400

    Preloader Updates

--- jquery.preloader.js
+++ jquery.preloader.js
@@ -1,17 +1,80 @@
 var Loading = {
-	show: function() { 
-		$("body").prepend(
-			$('<div id="preloader">').append(
-				$('<div id="status">').html("&nbsp;")
-			) 
-		);
-		$("#preloader").show();
-		$('#status').show();
-		//$('body').css({'overflow':'hidden'});
-	}, 
-	hide: function() {
-		$('#status').fadeOut();
-		$('#preloader').delay(5).fadeOut('slow').remove();; 
-		$('body').delay(5).css({'overflow':'visible'});
-	}
+    show: function() {
+        $("body").prepend(
+            $('<div id="preloader">').append(
+                $('<div id="status">').html("Loading...").append(
+                    $('<span class="percentage">')
+                )
+            )
+        );
+        $("#preloader").show();
+        $('#status').show();
+        //$('body').css({'overflow':'hidden'});
+    },
+    hide: function() {
+        setTimeout(function() {
+            $('#status').fadeOut();
+            $('#preloader').delay(5).fadeOut('slow').remove();;
+            $('body').delay(5).css({ 'overflow': 'visible' });
+        }, Preloader.preload_images.length * 300);
+
+    }
+}
+
+$(document).ready(function() {
+    $('html, body').css({ 'overflow': 'hidden' });
+    Preloader.init();
+});
+
+var Preloader = {
+    images_loaded: 0,
+    preloader_loaded_percentage: 0,
+    preload_images: [
+        "_assets/images/beercan-ballsfalls.png",
+        "_assets/images/beercan-twentymile.png",
+        "_assets/images/beercans.png",
+        "_assets/images/bknd-findus.jpg",
+        "_assets/images/bknd-home.jpg",
+        "_assets/images/bknd-locations.jpg",
+        "_assets/images/bknd-ourbeer-slideover-ballsfalls.jpg",
+        "_assets/images/bknd-ourbeer-slideover-twentymile.jpg",
+        "_assets/images/bknd-ourbeer.jpg",
+        "_assets/images/bknd-ourbrewery.jpg",
+        "_assets/images/bknd-ourhome.jpg",
+        "_assets/images/bknd-ourpeople.jpg",
+        "_assets/images/bknd-ourregion.jpg",
+        "_assets/images/bknd-ourvision.png"
+    ],
+    init: function() {
+        var self = this;
+        // Show preload screen 
+        Loading.show();
+
+        console.log("Loading ", self.preload_images.length, " Images");
+
+        $.each(self.preload_images, function(i, image) {
+            var img = new Image();
+
+            $(img).load(function() {
+                self.images_loaded++;
+                self.preloader_loaded_percentage = self.images_loaded * 100 / self.preload_images.length;
+
+                console.log("Loaded", self.images_loaded, " : ", self.preloader_loaded_percentage, "%");
+
+                $("#preloader span.percentage").animate({
+                    width: self.preloader_loaded_percentage + "%"
+                }, 250);
+
+                // When Image Is Loaded
+                if (self.images_loaded === self.preload_images.length) {
+                    console.log("Images Loaded");
+                    $("#preloader span.percentage").animate({
+                        width: "100%"
+                    }, 10)
+                    Loading.hide();
+                }
+            })
+            img.src = image;
+        });
+    }
 }
